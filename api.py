0bab6db53afb5bd937cb0da1d6ae2593b12ebdbf
liaamg1 <lgustafsson71@gmail.com>
2024-09-18 14:43:49 +0200

    commit

--- api.py
+++ api.py
@@ -1,1 +1,13 @@
 #här ska vi skapa en rest api till projektet
+
+#Grundkod för att testa att flask fungerar
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, Flask!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 # Main filen där vi skriver kommandon som ska exekveras
 # Vi kan fortfarande arbeta grafiskt med hjälv av tkinter eller andra bibliotek
+
