51436a8efc1788641df9acd9a4832438b947db81
liaamg1 <lgustafsson71@gmail.com>
2024-10-01 09:44:28 +0200

    testar GET med några burgare som ligger i api filen, dessa ska ligga i sql databasen senare

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 #här ska vi skapa en rest api till projektet
 
 #Grundkod för att testa att flask fungerar
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, Flask!'
+burgers = [
+    {"id": 1, "name": "Cheeseburger", "price": 5.99, "description": "A delicious cheeseburger."},
+    {"id": 2, "name": "Bacon Burger", "price": 6.99, "description": "A juicy burger topped with crispy bacon."},
+    {"id": 3, "name": "Veggie Burger", "price": 4.99, "description": "A hearty veggie burger."}
+]
+
+orders = []  # To store orders
+
+@app.route('/api/burgers', methods=['GET'])
+def get_burgers():
+    return jsonify(burgers)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
