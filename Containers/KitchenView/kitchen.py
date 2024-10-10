1e2dd1bd70298434685fc02ec98cdee86f41c206
Levinbandrez <Levin.raffati@gmail.com>
2024-10-10 14:34:58 +0200

    upd kod

--- Containers/KitchenView/kitchen.py
+++ Containers/KitchenView/kitchen.py
@@ -3,7 +3,26 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# g´hello
+received_orders = []
+
+def receive_order():
+    order = request.json
+
+    if not order or 'burger' not in order or 'ingredients' not in order:
+        return jsonify({"message": "Invalid order format"}), 400
+
+    Received_orders.append(order)
+
+    print("\nNew Order Received!")
+    print(f"Burger: {order['burger']}")
+    print(f"Ingredients: {', '.join(order['ingredients'])}")
+    print("-" * 30)
+
+    return jsonify({"message": "Order received successfully!"}), 200
+
+def view_orders():
+        return jsonify(received_orders)
+
 
 if __name__ == '__main__':
     app.run(port=5001)
