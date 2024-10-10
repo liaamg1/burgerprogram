3b523f12681a580544da8dc51cf097535d041a12
jismark <hugo.jismark@hotmail.com>
2024-10-10 12:42:50 +0200

    Början på databas

--- databas.sql
+++ databas.sql
@@ -1,3 +1,17 @@
 # Databasen som används i projektet
 # Vi kommer behöva koppla denna databas till vårt projekt till main filen
 # Vi kan också använda oss av json filer för att spara data istället för en SQL databas
+import mysql.connector
+
+db_connection = mysql.connector.connect(
+    host="localhost", # databasen ligger på localhost
+    user="root" # användarnamn
+    password="root" # lösenord
+    database="burgardatabase" # databasens namn
+)
+
+cursor = db_connection.cursor()
+
+cursor.execute(
+    
+)# SQL kommando för att skapa en tabell
