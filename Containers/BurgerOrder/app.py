04fec4b3958b8af2501c7505ded4fce7fa20f3a5
mansbackstrom <mans.backstrom@hotmail.se>
2024-10-17 21:19:43 +0200

    Engineering update in månsbäckström.txt

--- Containers/BurgerOrder/app.py
+++ Containers/BurgerOrder/app.py
@@ -29,12 +29,12 @@ def send_order():
 
     return jsonify(response.json()), response.status_code
 
-# Error handling
+# Invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
 
-# Error handling
+# Internal Server Error
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
